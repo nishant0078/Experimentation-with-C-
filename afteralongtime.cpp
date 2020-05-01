ca9d12d954824da56b276ecfddc21ff2d491c7ab
nishant0078 <nishant426@hotmail.com>
2020-05-02 04:17:44 +0530

    Basic Sum Program

---
+++ afteralongtime.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std ;
+
+int main ()
+{
+ float a ;
+
+ float b ; 
+
+ float sum ;
+
+a=20;
+
+b=40;
+
+sum= a+b;
+std::cout<<"The sum is"<< sum;
+
+return sum;
+
+}
+
+
